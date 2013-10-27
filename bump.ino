994b4f9cfe71c990050ad29d6e878970811244d3
Peter Teichman <peter@teichman.org>
2013-10-27 14:00:33 -0700

    Play at 120bpm if there isn't a MIDI clock signal

--- bump.ino
+++ bump.ino
@@ -3,7 +3,7 @@
 
 #include <MsTimer2.h>
 
-const int BEATS = 16;
+const int STEPS = 16;
 
 const int LED_PIN = 13;
 const int BUF_LEN = 64;
@@ -15,14 +15,19 @@ char beat3[BUF_LEN];
 void setup() {
     pinMode(LED_PIN, OUTPUT);
     MIDI.begin(0);
-  
+
     randomSeed(analogRead(0));
-    
-    fill_beats((char *)&beat1, (int)random(BEATS), BEATS);
-    fill_beats((char *)&beat2, (int)random(BEATS), BEATS);
-    fill_beats((char *)&beat3, (int)random(BEATS), BEATS);
-    
-    MIDI.setHandleClock(&on_clock);
+
+    fill_beats((char *)&beat1, (int)random(STEPS), STEPS);
+    fill_beats((char *)&beat2, (int)random(STEPS), STEPS);
+    fill_beats((char *)&beat3, (int)random(STEPS), STEPS);
+
+    // set up a timer running at 120bpm
+    MsTimer2::stop();
+    MsTimer2::set((int)(2500. / 120), &on_tick);
+    MsTimer2::start();
+
+    MIDI.setHandleClock(&on_first_midi_tick);
 }
 
 void fill_beats(char *beat, int num_beats, int num_steps) {
@@ -39,9 +44,17 @@ void fill_beats(char *beat, int num_beats, int num_steps) {
     }
 }
 
+void on_first_midi_tick() {
+    on_tick();
+
+    // disable the automatic timer and switch over to the MIDI clock
+    MIDI.setHandleClock(&on_tick);
+    MsTimer2::stop();
+}
+
 /* 24 pulses per quarter note: so for 8th notes wait 12 pulses */
 volatile unsigned char clock = 0;
-void on_clock() {
+void on_tick() {
     if (++clock > 12) {
         on_beat();
         clock = 0;
@@ -53,18 +66,20 @@ void on_beat() {
     MIDI.sendNoteOn(36, 0, 1);
     MIDI.sendNoteOn(38, 0, 1);
     MIDI.sendNoteOn(51, 0, 1);
-    
+
     if (beat1[pos] == 'x') {
         MIDI.sendNoteOn(36, 100, 1);
     }
+
     if (beat2[pos] == 'x') {
         MIDI.sendNoteOn(38, 100, 1);
     }
+
     if (beat3[pos] == 'x') {
         MIDI.sendNoteOn(51, 100, 1);
     }
 
-    if (++pos == BEATS) {
+    if (++pos == STEPS) {
         pos = 0;
     }
 }
